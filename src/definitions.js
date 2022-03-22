0f387c1ad7f11bb594dd7e76573ccd1757e4d3c8
Derek Chancellor <me@chancellor.tech>
2022-03-22 03:07:16 -0500

    Implementing corner sweeps

--- src/definitions.js
+++ src/definitions.js
@@ -8,6 +8,7 @@ export const functionButton = {
 };
 
 export const cornerButtons = [11, 81, 88, 18];
+
 export const centerButtons = [54, 55, 44, 45];
 
 export const hexRef = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
--- src/plays.js
+++ src/plays.js
@@ -1,32 +1,70 @@
 import { chooseRandomCorner, generateRandomColor } from './helpers.js';
 
-let isPlayRunning = false;
+let isPlayRunning;
+let play;
 
-const cornerSweep = (board) => {
-  isPlayRunning = true;
-  console.log('corner sweep');
-  const origin = chooseRandomCorner();
-  const color = generateRandomColor();
+const usedButtons = new Set();
 
-  const doTheThing = (number) => {
-    board.setButtonColor(origin, color);
-    // Add 1 to the number and light
-    // Subtract 1 from the number and light
-    // Add 10 to the number and light
-    // Subtract 10 to the number and light
-    // For each scenario, check if off the board, or already lit by this function
-  };
-
-  doTheThing();
-  return origin;
+const edgeButtons = [19, 20, 29, 30, 39, 40, 49, 50, 59, 60, 69, 70, 79, 80, 89, 90];
+const doTheThing = (button) => {
+  const newLights = [];
+  if (!usedButtons.has(button + 1) && isInBoundary(button + 1)) newLights.push(button + 1);
+  if (!usedButtons.has(button + 10) && isInBoundary(button + 10)) newLights.push(button + 10);
+  if (!usedButtons.has(button - 1) && isInBoundary(button - 1)) newLights.push(button - 1);
+  if (!usedButtons.has(button - 10) && isInBoundary(button - 10)) newLights.push(button - 10);
+  return newLights;
 };
 
-const firework = () => {
+const isInBoundary = (button) => {
+  if (button > 88 || button < 11 || edgeButtons.includes(button)) return false;
+  return true;
+};
+
+const cornerSweep = (board) => {
+  stopPlay(board, false);
+  console.log('corner sweep');
+  isPlayRunning = true;
+  const origin = chooseRandomCorner();
+  const color = generateRandomColor();
+  board.setButtonColor(origin, color);
+
+  let activeButtons = [];
+  activeButtons.push(origin);
+
+  let count = 0;
+  play = setInterval(() => {
+    count += 1;
+    console.log('play #', count);
+    let newButtons = [];
+    activeButtons.forEach((activeButton) => {
+      newButtons = [...newButtons, ...doTheThing(activeButton)];
+    });
+    console.log(newButtons);
+    usedButtons.add(...activeButtons);
+    console.log('used...', usedButtons);
+    // if (newButtons.length === 0) stopPlay(board, false);
+    // console.log(newButtons);
+    newButtons.forEach((button) => {
+      board.setButtonColor(button, color);
+    });
+    activeButtons = newButtons;
+  }, 500);
+};
+
+const stopPlay = (board, shouldClearBoard) => {
+  console.log('stopping...');
+  if (isPlayRunning) clearInterval(play);
+  if (shouldClearBoard) board.allOff();
+  isPlayRunning = false;
+};
+
+const firework = (board) => {
   isPlayRunning = true;
   console.log('fireworks');
 };
 
 const plays = {
+  stop: (board, shouldClearBoard) => stopPlay(board, shouldClearBoard),
   fireworks: (board) => firework(board),
   cornerSweep: (board) => cornerSweep(board),
 };
